b8dfead0daf76a4e58ebfaff5365bf2d5c0955b8
yz-apr <zhouyi0426@hotmail.com>
2026-04-17 13:47:44 +0900

    Update EM.R

--- 2/EM.R
+++ 2/EM.R
@@ -1,11 +1,13 @@
-x1 <- 5
+x1 <- 2
 tol <- 1e-6
 diff <- Inf
 
 while (diff > tol) {
   x2 <- (0 + 2 + x1) / 3
+  # x2 <- var(c(0,2,x1))
   diff <- abs(x2 - x1)
   x1 <- x2
+  print(x1)
 }
 
 x1
@@ -14,7 +16,7 @@ x <- 0
 tol <- 1e-6
 
 repeat {
-  x_new <- (2 + x) / 3
+  x_new <- var(c(0,2,x))
   if (abs(x_new - x) < tol) break
   x <- x_new
 }
